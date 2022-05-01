e54034da99c5a96c7464489f6a15fc23eb87f68b
Kris Urbanski <przemek.kris.urbanski@gmail.com>
2022-05-01 18:25:16 +0200

    Add update to prompt, new spell in diagnostics

--- modules/prompt.sh
+++ modules/prompt.sh
@@ -14,17 +14,18 @@ if [ "$color_prompt" = yes ]; then
 
     prompt_color='\[\033[;32m\]'
     info_color='\[\033[1;34m\]'
+    command_color='\[\033[1;33m\]'
     # prompt_symbol=🦧
     # prompt_symbol=🐻‍❄️
     prompt_symbol=🐻
     if [ "$EUID" -eq 0 ]; then # Change prompt colors for root user
         prompt_color='\[\033[;94m\]'
-        info_color='\[\033[1;31m\]'
+        info_color='\[\033[1;34m\]'
         prompt_symbol=💀
     fi
     case "$PROMPT_ALTERNATIVE" in
         twoline)
-            PS1=$prompt_color'\n┌──${debian_chroot:+($debian_chroot)──}${VIRTUAL_ENV:+(\[\033[0;1m\]$(basename $VIRTUAL_ENV)'$prompt_color')}('$info_color'\u${prompt_symbol}\h'$prompt_color')-[\[\033[0;1m\]\w'$prompt_color']\n│ $(__git_prompt)\n│ $(__watson_prompt)\n'$prompt_color'└─'$info_color'\$\[\033[0m\] ';;
+            PS1=$prompt_color'\n┌──${debian_chroot:+($debian_chroot)──}${VIRTUAL_ENV:+(\[\033[0;1m\]$(basename $VIRTUAL_ENV)'$prompt_color')}('$info_color'\u${prompt_symbol}\h'$prompt_color')-[\[\033[0;1m\]\w'$prompt_color']\n│ $(__git_prompt)\n│ $(__watson_prompt)\n'$prompt_color'└──'$info_color'\$ '$command_color;;
         oneline)
             PS1='${VIRTUAL_ENV:+($(basename $VIRTUAL_ENV)) }${debian_chroot:+($debian_chroot)}'$info_color'\u@\h\[\033[00m\]:'$prompt_color'\[\033[01m\]\w\[\033[00m\]\$ ';;
         backtrack)
